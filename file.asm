a68ace338b176102ae21fe5a6b3462a95efbc8ad
becauro <michelbecauro@gmail.com>
2023-09-04 18:56:13 -0300

    FEAT: Add username size controler

--- file.asm
+++ file.asm
@@ -26,16 +26,21 @@ question:
 
 user_answer:
 	mov bx, buf_user_answer 
+	mov cx, 0 		; counter for username size
 
 	.loop_user_answer:
-	mov ax, 0
-	int 0x16
-	mov [bx], al		
-	cmp byte [bx], 0xd
+
+	mov ax, 0      		; read char FUNC (active keyboard) 
+	cmp cx, 11        	; check username size
+	jg finish_question    	; jump if the size > 11	 
+	int 0x16    		; keyboard INT (copy input into 'al') 
+	mov [bx], al		; copy keyboard input to buffer	
+	cmp byte [bx], 0xd	; check if the enter key was pressed
 	je finish_question 
-	mov ah, 0x0e
-	int 0x10
-	inc bx
+	mov ah, 0x0e 		; write FUNC (active 'al' printer) 
+	int 0x10  		; video INT (copy 'al' into video) 
+	inc bx 			; set pointer to the next buffer address
+	inc cx			; control the username size
 	jmp .loop_user_answer
 
 finish_question:
@@ -55,7 +60,7 @@ end:
 	jmp $
 	
 buf_os_message: db 0xa, "Starting the Operating System...", 0, 
-buf_question: db 0xa,0xa, 0xd, "Now, type your username here [max size: 10 char] >>: ", 0
+buf_question: db 0xa,0xa, 0xd, "Now, type your username here [max size: 11 char] >>: ", 0
 buf_user_answer: times 11 db 0
 buf_finish_question: db 0xa, 0xa, 0xd, "FINISHED", 0
 
